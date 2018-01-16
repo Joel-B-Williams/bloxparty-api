0d724422c4d213a7e25653d16c005ced3d378bdb
Joel <joel.b.williams.is@gmail.com>
2018-01-16 10:04:26 -0600

    post to create playpen

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 
 let pool = new pg.Pool({
 	port: process.env.DB_PORT || 5432,
-	password: process.env.Db_PW,
+	password: process.env.DB_PW,
 	database: process.env.DB,
 	user: process.env.DB_USER || 'postgres',
 	host: 'localhost',
@@ -25,5 +25,27 @@ app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
+
+app.post('/api/new-playpen', function(req,res) {
+	
+	var playpen_name = req.body.name;
+
+	pool.connect((err,client,done)=>{
+		if(err){
+			console.log('first',err)
+			return res.status(400).send(err);
+		} else {
+			client.query('INSERT INTO playpens (name) VALUES ($1)',[playpen_name],(err,table)=>{
+				if(err){
+					console.log('second', err)
+					return res.status(400).send(err);
+				} else {
+					console.log(table);
+				}
+			});
+		}
+
+	});
+})
 
 app.listen(PORT, ()=> console.log('Listening on port ' + PORT));
